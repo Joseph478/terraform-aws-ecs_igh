7260e36ab7ae22834f6bc9df0936efac426803e5
Joseph <josehparedes255@gmail.com>
2026-05-18 17:29:57 -0500

    feat(outputs): anadiendo outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,39 @@ output "id_ecs_service" {
 output "name_ecs_service" {
     value = aws_ecs_service.ecs_service.name
 }
+
+output "url_ecr_repository" {
+    value = aws_ecr_repository.ecr_repository.repository_url
+}
+
+output "registry_id_ecr" {
+    value = aws_ecr_repository.ecr_repository.registry_id
+}
+
+output "id_ecs_cluster" {
+    value = aws_ecs_cluster.ecs_cluster.id
+}
+
+output "arn_ecs_task_definition" {
+    value = aws_ecs_task_definition.task_definition.arn
+}
+
+output "revision_ecs_task_definition" {
+    value = aws_ecs_task_definition.task_definition.revision
+}
+
+output "arn_ecs_task_role" {
+    value = aws_iam_role.ecs_task_role.arn
+}
+
+output "name_ecs_task_role" {
+    value = aws_iam_role.ecs_task_role.name
+}
+
+output "name_cloudwatch_log_group" {
+    value = aws_cloudwatch_log_group.ecs_tasks.name
+}
+
+output "arn_cloudwatch_log_group" {
+    value = aws_cloudwatch_log_group.ecs_tasks.arn
+}
